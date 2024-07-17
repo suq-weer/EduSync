7bd8756b8c903f48c5355f6c0e378d77676e30f8
LLP <lilianpei0710@163.com>
2024-07-17 17:35:37 +0800

    jz要的大按钮

--- client/main.py
+++ client/main.py
@@ -1,5 +1,16 @@
 from client.event import StatusUploadEvent
+from PySide6.QtWidgets import QApplication. QMainWindow
+from ui_untitled import Ui_MainWindow
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self),__init__()
+
 
 if __name__ == "__main__":
     event_status = StatusUploadEvent(1, 'status_bus', 1)
     event_status.start()
+    app = QApplication([])
+    window = MainWindow()
+    window.show()
+    app.exec()
