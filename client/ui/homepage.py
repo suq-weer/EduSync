a020e05ace339e2a084d54a7d23507319ed715a4
LLP <lilianpei0710@163.com>
2024-08-09 19:41:06 +0800

    iu更新，把jz要的大按钮变成圆形

--- client/ui/homepage.py
+++ client/ui/homepage.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'homepage.ui'
+## Form generated from reading UI file 'homepageYmMjWi.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.7.2
 ##
@@ -15,11 +15,11 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QLabel, QMainWindow, QMenuBar,
-    QPlainTextEdit, QSizePolicy, QStatusBar, QToolButton,
-    QWidget)
+from PySide6.QtWidgets import (QApplication, QLabel, QListWidget, QListWidgetItem,
+    QMainWindow, QSizePolicy, QWidget)
 
-from qfluentwidgets import PushButton
+from qfluentwidgets import (MenuBar, PlainTextEdit, PushButton, StatusBar,
+    ToolButton)
 
 class Ui_EduSync(object):
     def setupUi(self, EduSync):
@@ -28,27 +28,37 @@ class Ui_EduSync(object):
         EduSync.resize(504, 352)
         self.centralwidget = QWidget(EduSync)
         self.centralwidget.setObjectName(u"centralwidget")
-        self.plainTextEdit = QPlainTextEdit(self.centralwidget)
+        self.plainTextEdit = PlainTextEdit(self.centralwidget)
         self.plainTextEdit.setObjectName(u"plainTextEdit")
         self.plainTextEdit.setGeometry(QRect(0, 0, 501, 41))
-        self.plainTextEdit_2 = QPlainTextEdit(self.centralwidget)
-        self.plainTextEdit_2.setObjectName(u"plainTextEdit_2")
-        self.plainTextEdit_2.setGeometry(QRect(0, 40, 501, 271))
-        self.toolButton = QToolButton(self.centralwidget)
+        self.toolButton = ToolButton(self.centralwidget)
         self.toolButton.setObjectName(u"toolButton")
         self.toolButton.setGeometry(QRect(460, 10, 24, 21))
         self.pushButton = PushButton(self.centralwidget)
         self.pushButton.setObjectName(u"pushButton")
         self.pushButton.setGeometry(QRect(140, 80, 200, 200))
+        self.pushButton.setMinimumSize(QSize(200, 200))
+        self.pushButton.setMaximumSize(QSize(200, 200))
+        self.pushButton.setStyleSheet(u"border-radius:100px;\n"
+"background-color: rgb(222, 222, 222);")
+        self.pushButton.setInputMethodHints(Qt.InputMethodHint.ImhNone)
         self.label = QLabel(self.centralwidget)
         self.label.setObjectName(u"label")
         self.label.setGeometry(QRect(220, 10, 41, 21))
+        self.listWidget = QListWidget(self.centralwidget)
+        self.listWidget.setObjectName(u"listWidget")
+        self.listWidget.setGeometry(QRect(0, 40, 501, 251))
         EduSync.setCentralWidget(self.centralwidget)
-        self.menubar = QMenuBar(EduSync)
+        self.listWidget.raise_()
+        self.plainTextEdit.raise_()
+        self.toolButton.raise_()
+        self.pushButton.raise_()
+        self.label.raise_()
+        self.menubar = MenuBar(EduSync)
         self.menubar.setObjectName(u"menubar")
         self.menubar.setGeometry(QRect(0, 0, 504, 33))
         EduSync.setMenuBar(self.menubar)
-        self.statusbar = QStatusBar(EduSync)
+        self.statusbar = StatusBar(EduSync)
         self.statusbar.setObjectName(u"statusbar")
         EduSync.setStatusBar(self.statusbar)
 
