2cbaecce4ea4eefd48e5778a3285f4b0c7ce3011
LLP <lilianpei0710@163.com>
2024-10-03 13:23:37 +0800

    ui(3.0)更新

--- client/main.py
+++ client/main.py
@@ -1,3 +1,14 @@
-from event import StatusUploadEvent
+import sys
+from PySide6.QtWidgets import  QApplication, QMainWindow
+from ui.homepage import Ui_EduSync
 
+'''
+from event import StatusUploadEvent
 StatusUploadEvent(1, 'status_bus', 1).start()
+'''
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ui = Ui_EduSync
+    ui.show()
+    sys.exit(app.exec())
