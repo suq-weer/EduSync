43d6be4b5af2ebef7693154a7ad31a5c1f08e4af
Xiaosu <3076878009@qq.com>
2024-10-04 12:21:31 +0800

    修改顶边栏，添加<Routerlink />

---
+++ manager/src/components/NavigationView.vue
@@ -0,0 +1,30 @@
+<template>
+  <ul>
+    <li href="#"><RouterLink to="/">主页</RouterLink></li>
+  </ul>
+  <br />
+</template>
+<style>
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  border-top-style: solid;
+  border-top-color: blue;
+  border-bottom-style: solid;
+  border-bottom-color: blue;
+}
+li {
+  float: left;
+}
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  text-decoration: none;
+  padding: 14px 16px;
+}
+li a:hover {
+  color: blue;
+}
+</style>
