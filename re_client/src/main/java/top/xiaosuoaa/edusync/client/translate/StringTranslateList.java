91a51a0bdb8361c1a6c860a591f433ca0ab0671b
Xiaosu <3076878009@qq.com>
2024-10-20 09:34:36 +0800

    提交重构后UI

---
+++ re_client/src/main/java/top/xiaosuoaa/edusync/client/translate/StringTranslateList.java
@@ -0,0 +1,6 @@
+package top.xiaosuoaa.edusync.client.translate;
+
+public record StringTranslateList() {
+	public static final String START_SERVICE = "启动服务";
+	public static final String END_SERVICE = "结束服务";
+}
