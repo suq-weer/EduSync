7dbb6f6e8dc2f1d74083483bff348dffd6666c14
Xiaosu <3076878009@qq.com>
2024-10-20 09:34:36 +0800

    提交重构后UI

--- re_client/src/main/java/module-info.java
+++ re_client/src/main/java/module-info.java
@@ -1,10 +1,10 @@
-module top.xiaosuoaa.edusync.client.re_client {
+module top.xiaosuoaa.edusync.client {
     requires javafx.controls;
     requires javafx.fxml;
 
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
 
-    opens top.xiaosuoaa.edusync.client.re_client to javafx.fxml;
-    exports top.xiaosuoaa.edusync.client.re_client;
+    opens top.xiaosuoaa.edusync.client to javafx.fxml;
+    exports top.xiaosuoaa.edusync.client;
 }
--- re_client/src/main/java/top/xiaosuoaa/edusync/client/HomeApplication.java
+++ re_client/src/main/java/top/xiaosuoaa/edusync/client/HomeApplication.java
@@ -1,4 +1,4 @@
-package top.xiaosuoaa.edusync.client.re_client;
+package top.xiaosuoaa.edusync.client;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class HomeApplication extends Application {
 	@Override
 	public void start(Stage stage) throws IOException {
-		FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-		Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-		stage.setTitle("Hello!");
+		FXMLLoader fxmlLoader = new FXMLLoader(HomeApplication.class.getResource("home-view.fxml"));
+		Scene scene = new Scene(fxmlLoader.load(), 640, 340);
+		stage.setTitle("EduSync");
 		stage.setScene(scene);
 		stage.show();
 	}
