a808536281ba48417cfdb43b7b088c311cb083a0
xiaoyi <2132539636@qq.com>
2024-11-03 21:45:38 +0800

    login开发完毕

--- manager/src/api/server.ts
+++ manager/src/api/server.ts
@@ -1,7 +1,5 @@
-import instance from '../utils/request'
+import instance from '@/utils/request'
 import type { UnwrapRef } from 'vue'
-import { cookie_write_user } from './manage.ts'
-import { alert } from 'mdui/functions/alert.js'
 
 export const postLogin = async (body: { uid: UnwrapRef<string>, password: UnwrapRef<string> }) => {
   try {
@@ -12,30 +10,23 @@ export const postLogin = async (body: { uid: UnwrapRef<string>, password: Unwrap
     return err
   }
 }
+export const postKey = async (body: { uid: UnwrapRef<string>, key: UnwrapRef<string> }) => {
+  try {
+    const response = await instance.post('/api/if_key.php', body)
+    return response.data
+  } catch (err) {
+    console.error(err)
+    return err
+  }
+}
 
 export const Login = async (uid: string, password: string) => {
+
   // console.log({ uid: uid.value, password: pass.value })
-  if (uid&&password) {
-    const result = await postLogin({ uid: uid, password: password })
-    console.log(result)
+    return await postLogin({ uid: uid, password: password})
 
-    //判断结果
-    if (result['states'] === 1) {
+}
 
-      await cookie_write_user({ uid: uid, pass: password })
-      this.$router.replace('home')
-
-    } else {
-
-      alert({
-        headline: document.title,
-        description: result['data'],
-        confirmText: "好",
-        onConfirm: () => password = '',
-      })
-
-    }
-  }
-  else alert({ headline: document.title, description: "请输入完整!", confirmText: "好" })
-
+export const is_valid_key = async (uid: string, key: string) => {
+  return await postKey({ uid: uid, key: key })
 }
