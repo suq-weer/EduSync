12fbac5fc625cde831a673474aa0e028e7682ee0
xiaoyi <2132539636@qq.com>
2025-01-30 21:23:13 +0800

    巴拉巴拉

---
+++ manager/src/components/DeviceListElement.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+  <mdui-card class="deviceListView_card">
+
+  </mdui-card>
+</template>
+
+
+<style scoped>
+.deviceListView_card {
+  display: block;
+  background: rgba(var(--mdui-color-secondary-container));
+  width: 100%;
+  height: auto;
+}
+</style>
